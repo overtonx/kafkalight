97c1abc3e25c44245ed7216897503c2f94a3a4d7
radionov <andrey.radionoff@gmail.com>
2025-09-22 07:21:55 +0500

    refactor tracing middleware to use OpenTelemetry's default propagator

--- middleware/tracing.go
+++ middleware/tracing.go
@@ -3,15 +3,16 @@ package middleware
 import (
 	"context"
 
-	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 
 	"github.com/overtonx/kafkalight"
 )
 
-func Tracing(propagator propagation.TextMapPropagator) kafkalight.Middleware {
+func Tracing() kafkalight.Middleware {
 	return func(next kafkalight.MessageHandler) kafkalight.MessageHandler {
 		return func(ctx context.Context, msg *kafkalight.Message) error {
+			propagator := otel.GetTextMapPropagator()
 			ctx = propagator.Extract(ctx, kafkalight.NewMessageCarrier(msg))
 
 			span := trace.SpanFromContext(ctx)
